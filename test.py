00c49d20f6d8993635decf1b0c06a2f00a3fe281
William <wokora@gmail.com>
2024-09-26 22:48:49 +0300

    updates

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import calendar
 start_month = 2  # May
 total_years = 2  # Number of years (including the first partial year)
 total_months = 12 * total_years  # Total months to loop through
-print(total_months)
 
 # Loop through all months starting from the specified month
 for i in range(total_months):
@@ -16,7 +15,5 @@ for i in range(total_months):
     if year > total_years:
         break
     # # Get the month name
-    # month_name = calendar.month_name[month_num]
-    # print(f"Year {year}, Month {month_num}: {month_name}")
-
-    print(i, month_num, year)
+    month_name = calendar.month_name[month_num]
+    print(f"Year {year}, Month {month_num}: {month_name}")
