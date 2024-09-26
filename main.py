6f0338dc1bda76ac73b3726bc2c12614467b5e8d
William <wokora@gmail.com>
2024-09-26 23:38:35 +0300

    updates

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-years = 3
+years = 1
 months = 12
 total_months = years * months
-start_month = 1
-investment = 1420000
+complete_years = False
+start_month = 11
+investment = 50000
 interest_rate_percent = 14
 interest_rate = interest_rate_percent / 100
 monthly_interest_rate = interest_rate / 12
 total_interest = 0
-withdrawal = 0
+withdrawal = 400
 total_withdrawal = 0
+
 
 balance = investment
 break_down = "Y"
 break_down_string = "Year" if break_down == "Y" else "Month"
 
-# def calculate_interest(investment):
-#     interest = investment * monthly_interest_rate
-#     balance = investment + interest
-#     balance = balance - withdrawal
-#     investment = balance
-    
-#     return  interest, investment, balance
+if complete_years is True:
+    start_month = 1
     
 
 print(f"{break_down_string} \t Interest \t\t Total Interest \t Balance \t Withdrawal")
@@ -29,9 +26,12 @@ print(f"----- \t -------- \t\t --------------- \t ------- \t -----------")
 
 for i in range(total_months):
     month = (start_month + i - 1) % 12 + 1
+    if complete_years is True:
+        month = i + 1
+
     year = 1 + (start_month + i - 1) // 12
 
-    if year > years:
+    if complete_years is False and year > years:
         break
 
     interest = investment * monthly_interest_rate
